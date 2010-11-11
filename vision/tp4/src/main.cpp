41b3d2940b26802c7f774437969711001a64de68
Moreno Trlin <moreno@aldebaran.(none)>
2010-11-11 23:01:09 +0100

    il est tard

--- vision/tp4/src/main.cpp
+++ vision/tp4/src/main.cpp
@@ -23,32 +23,37 @@ void nMeilPts(char* char0,char * char1,char * char2,double alpha){
   im.EcrireImagePGM(char2);
 }
 
-void test_matchPoints(char * fic1,char* fic2,double alpha,int winn,int winp){
+void test_matchPoints(double alpha,int winn,int winp,char * fic1,char* fic2){
   image im1(fic1);
   image comp(fic2);
-  im1.matchPoints(comp,50,winn,winp,&image::zncc);
+  im1.matchPoints(comp,50,winn,winp,&image::ssd,false);
 }
 
-void debil(char * fic,char * fic2){
-  image im(fic);
-  image* gaussien =im.GaussFilter();
-  gaussien->EcrireImagePGM(fic2);
+void test_drawMatchPoints(double alpha,int winn,int winp,char * fic1,char* fic2,char * fic_s){
+  image im1(fic1);
+  image im2(fic2);
+
+  image* sortie=im1.drawDblMatchPoints(im2,100,winn,winp,&image::zncc,true);
+  sortie->EcrireImagePGM(fic_s);
 }
+
 
 int main(int argc, char* argv[]){
 
   double alpha;
-  sscanf(argv[2],"%f",&alpha);
+  sscanf(argv[1],"%f",&alpha);
 
   //exHarris(argv[1],alpha,argv[3]);
 
   //nMeilPts(argv[1],argv[3],argv[4],alpha);
-   image im1(argv[1]);
-   image im2(argv[2]);
-   image im(im1,im2);
+  // image im1(argv[1]);
+  // image im2(argv[2]);
+  // image im(im1,im2);
    //image sortie(im.getHauteur(),im.getLargeur(),im.getValmax());
-    im.EcrireImagePGM(argv[3]);
-  //test_matchPoints(argv[1],argv[3],alpha,atoi(argv[4]),atoi(argv[5]));
+   //im.EcrireImagePGM(argv[3]);
+  //test_matchPoints(alpha,atoi(argv[2]),atoi(argv[3]),argv[4],argv[5]);
+
+  test_drawMatchPoints(alpha,atoi(argv[2]),atoi(argv[3]),argv[4],argv[5],argv[6]);
 
   //////////////////////////////////
   //////////////////////////////////
