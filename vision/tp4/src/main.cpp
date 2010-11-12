f16e61446bb0c0e84e3be6838702244287666a3e
Moreno Trlin <tzmoreno@gmail.com>
2010-11-12 11:42:18 +0100

    rien

--- vision/tp4/src/main.cpp
+++ vision/tp4/src/main.cpp
@@ -33,7 +33,15 @@ void test_drawMatchPoints(double alpha,int winn,int winp,char * fic1,char* fic2,
   image im1(fic1);
   image im2(fic2);
 
-  image* sortie=im1.drawDblMatchPoints(im2,100,winn,winp,&image::zncc,true);
+  image* sortie=im1.drawMatchPoints(im2,50,winn,winp,&image::zncc,true);
+  sortie->EcrireImagePGM(fic_s);
+}
+
+void test_drawDblMatchPoints(double alpha,int winn,int winp,char * fic1,char* fic2,char * fic_s){
+  image im1(fic1);
+  image im2(fic2);
+
+  image* sortie=im1.drawDblMatchPoints(im2,50,winn,winp,&image::zncc,true);
   sortie->EcrireImagePGM(fic_s);
 }
 
@@ -45,12 +53,12 @@ int main(int argc, char* argv[]){
 //   sscanf(argv[2],"%f",&alpha);
 // //   int nb_interest_pts = atoi(argv[3]);
 // //   nMeilPts(argv[1],argv[4],alpha,nb_interest_pts);
-//  
-//   
+//
+//
 //   exHarris(argv[1],alpha,argv[3]);
-// 
-//   
-//   
+//
+//
+//
 //    //image im1(argv[1]);
 //    //image im2(argv[2]);
 //    //image im(im1,im2);
