f3787837a60be5d08ec5936b3bac459b1f0cde8a
Moreno Trlin <moreno@aldebaran.(none)>
2010-11-19 14:16:31 +0100

    le cercle est fait à tester...

--- geo_image/tp1/src/main.cpp
+++ geo_image/tp1/src/main.cpp
@@ -1,5 +1,5 @@
 #include "image.hpp"
-
+#include <cmath>
 // Cette fonction permet de creer une image représentant l'élément structurant qui sera
 // donc un carre de taille 'taille'
 image creerImCarre(int taille){
@@ -9,14 +9,28 @@ image creerImCarre(int taille){
 			sortie(i,j)=1;
 		}
 	}
-	return sortie; 
+	return sortie;
 }
+
+image elemCercle(int taille){
+  assert(taille%2 == 1);
+  image cercle(taille,taille,0);
+  double rayon=taille/2;
+
+  for(double i=0;i<taille;i++)
+    for(double j=0;j<taille;j++)
+      if((i-rayon)*(i-rayon)+(j-rayon)*(j-rayon)<rayon*rayon)cercle(i,j)=1;
+
+  return cercle;
+}
+
 
 void test_DupStructCarre(char* fichier_entree ,char* fichier_sortie,int taille_carre){
   image im(fichier_entree);
   image elem_structurant = creerImCarre(taille_carre);
   image* sortie=im.duplique_elemStruc_bord(elem_structurant);
   sortie->EcrireImagePGM(fichier_sortie);
+}
 
 void test_trous(const char * fic){
   image im(fic);
@@ -57,7 +71,7 @@ using namespace std;
   image* sortie=im.dilatation(elem_structurant);
   sortie->EcrireImagePGM("dilatation.pgm");
 
-  
-  
+
+
 return 0;
 }
