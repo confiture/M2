c69c935020e8e40c27aae6a158dc4fd7683ff351
Moreno Trlin <moreno@aldebaran.(none)>
2010-11-19 14:28:21 +0100

    retour à la normale

--- geo_image/tp1/src/main.cpp
+++ geo_image/tp1/src/main.cpp
@@ -1,5 +1,5 @@
 #include "image.hpp"
-
+#include <cmath>
 // Cette fonction permet de creer une image représentant l'élément structurant qui sera
 // donc un carre de taille 'taille'
 image creerImCarre(int taille){
@@ -9,8 +9,21 @@ image creerImCarre(int taille){
 			sortie(i,j)=1;
 		}
 	}
-	return sortie; 
+	return sortie;
 }
+
+image elemCercle(int taille){
+  assert(taille%2 == 1);
+  image cercle(taille,taille,0);
+  double rayon=taille/2;
+
+  for(double i=0;i<taille;i++)
+    for(double j=0;j<taille;j++)
+      if((i-rayon)*(i-rayon)+(j-rayon)*(j-rayon)<rayon*rayon)cercle(i,j)=1;
+
+  return cercle;
+}
+
 
 void test_DupStructCarre(char* fichier_entree ,char* fichier_sortie,int taille_carre){
   image im(fichier_entree);
@@ -18,6 +31,7 @@ void test_DupStructCarre(char* fichier_entree ,char* fichier_sortie,int taille_c
   image* sortie=im.duplique_elemStruc_bord(elem_structurant);
   sortie->EcrireImagePGM(fichier_sortie);
 }
+
 void test_trous(const char * fic){
   image im(fic);
   image neg(im);
@@ -51,25 +65,13 @@ void test_trous(const char * fic){
 int main(int argc, char* argv[]){
 using namespace std;
 
+  //test_DupStructCarre("objets.pgm","imgDuplique10.pgm",10);
+  image im("objets.pgm");
+  image elem_structurant = creerImCarre(5);
+  image* sortie=im.dilatation(elem_structurant);
+  sortie->EcrireImagePGM("dilatation.pgm");
 
-// Test dilatation
-//   image im("objets.pgm");
-//   image elem_structurant = creerImCarre(5);
-//   image* sortie=im.dilatation(elem_structurant);
-//   sortie->EcrireImagePGM("dilatationCarre7.pgm");
- 
-// Test erosion 
-  image im("objets.pgm");
-  image elem_structurant = creerImCarre(5);
-  image* sortie=im.erosion(elem_structurant);
-  sortie->EcrireImagePGM("erosionCarre7.pgm");
-  
-// Test ouverture  
-  image im("objets.pgm");
-  image elem_structurant = creerImCarre(5);
-  image* sortie=im.ouverture(elem_structurant);
-  sortie->EcrireImagePGM("erosionCarre7.pgm");
-  
-  
+
+
 return 0;
 }
