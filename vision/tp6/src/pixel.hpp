d46ada8d6e5124af9ca28737d256911357caccab
Moreno Trlin <moreno@aldebaran.(none)>
2010-11-23 12:17:18 +0100

    petite modif

--- vision/tp6/src/pixel.hpp
+++ vision/tp6/src/pixel.hpp
@@ -18,24 +18,24 @@ public:
 		valR=pix.valR;
 		valG=pix.valG;
 		valB=pix.valB;
-		
+
 		return (*this);
 	}
 
 	inline bool equalPos(const pixPPM & pix){
 		return i==pix.i && j==pix.j;
 	}
-	
+
 	inline double distance2(const pixPPM & pix_A, const pixPPM & pix_B){
-	  double res_i = (pix_A.i-pix_B.i)*(pix_A.i-pix_B.i);
-	  double res_j = (pix_A.j-pix_B.j)*(pix_A.j-pix_B.j);
-	  double res_R = (pix_A.valR-pix_B.valR)*(pix_A.valR-pix_B.valR);
-	  double res_G = (pix_A.valG-pix_B.valG)*(pix_A.valG-pix_B.valG);
-	  double res_B = (pix_A.valB-pix_B.valB)*(pix_A.valB-pix_B.valB);
-	  double res = res_i + res_j  + res_R + res_G + res_B;
-	  return res;
+		double res_i = (pix_A.i-pix_B.i)*(pix_A.i-pix_B.i);
+		double res_j = (pix_A.j-pix_B.j)*(pix_A.j-pix_B.j);
+		double res_R = (pix_A.valR-pix_B.valR)*(pix_A.valR-pix_B.valR);
+		double res_G = (pix_A.valG-pix_B.valG)*(pix_A.valG-pix_B.valG);
+		double res_B = (pix_A.valB-pix_B.valB)*(pix_A.valB-pix_B.valB);
+		double res = res_i + res_j  + res_R + res_G + res_B;
+		return res;
 	}
-	
+
 	inline  pixPPM operator+(const pixPPM & pix){
 		pixPPM res;
 		res.i = i + pix.i;
@@ -45,20 +45,20 @@ public:
 		res.valB = valB + pix.valB;
 		return res;
 	}
-	
+
 	inline pixPPM moyenne(const std::list<pixPPM> & Lpix){
-	  std::list<pixPPM>::const_iterator it=Lpix.begin();
-	  pixPPM moy(0,0,0,0,0);
-	  int nb_pix = 1;
-	  for(it;it!=Lpix.end();it++){
-	    moy = moy+(*it);
-	    nb_pix++;
-	  }
-	
-	return moy;
+		std::list<pixPPM>::const_iterator it=Lpix.begin();
+		pixPPM moy(0,0,0,0,0);
+		int nb_pix = 1;
+		for(it;it!=Lpix.end();it++){
+			moy = moy+(*it);
+			nb_pix++;
+		}
+
+		return moy;
 	}
-	
-	
+
+
 	int i;
 	int j;
 	double valR;
