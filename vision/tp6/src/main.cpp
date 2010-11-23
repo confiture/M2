03ec941efa9b78eb7198d34f45ee389e8f5847d0
Moreno Trlin <tzmoreno@gmail.com>
2010-11-23 11:58:15 +0100

    update pixel.hpp

--- vision/tp6/src/main.cpp
+++ vision/tp6/src/main.cpp
@@ -5,7 +5,21 @@ void testLecEcr(const char* fic1,const char* fic2){
   im.EcrireImagePPM(fic2);
 }
 
+ void testPixelAddition(){
+ pixPPM pixA(1,1,100,100,100);
+ pixPPM pixB;
+ pixB = pixA + pixA;
+ std::cout<<"i : "<<pixB.i<<"; ";
+ std::cout<<"j : "<<pixB.j<<"; ";
+ std::cout<<"R : "<<pixB.valR<<"; ";
+ std::cout<<"G : "<<pixB.valG<<"; ";
+ std::cout<<"B : "<<pixB.valB<<std::endl;
+ }
+ 
+
 int main(int argc, char* argv[]){
-  testLecEcr(argv[1],argv[2]);
+  //testLecEcr(argv[1],argv[2]);
+  testPixelAddition();
+  
   return 0;
 }
