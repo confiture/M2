49fd1fb7bff868fac190b648f7b0d4ab7bf2160d
Moreno Trlin <moreno@aldebaran.(none)>
2010-11-23 12:00:38 +0100

    main.cpp

--- vision/tp6/src/main.cpp
+++ vision/tp6/src/main.cpp
@@ -1,11 +1,26 @@
 #include "image.hpp"
+#include <list>
+
+using namespace std;
+
 
 void testLecEcr(const char* fic1,const char* fic2){
   image im(fic1);
   im.EcrireImagePPM(fic2);
 }
 
+void testInitCentroids(const char* fic1){
+  image im(fic1);
+  std::list<pixPPM> l=im.initCentroids(6);
+  list<pixPPM>::iterator it=l.begin();
+  for(it;it!=l.end();it++){
+    cout<<"i "<<it->i<<" j "<<it->j<<endl;
+  }
+
+}
+
 int main(int argc, char* argv[]){
-  testLecEcr(argv[1],argv[2]);
+  //testLecEcr(argv[1],argv[2]);
+  testInitCentroids(argv[1]);
   return 0;
 }
