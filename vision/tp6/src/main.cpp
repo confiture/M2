d2a571fc61f819570c5c2ba2ebc8673adb04f036
Moreno Trlin <tzmoreno@gmail.com>
2010-11-25 18:26:56 +0100

    bug

--- vision/tp6/src/main.cpp
+++ vision/tp6/src/main.cpp
@@ -28,9 +28,20 @@ void testLecEcr(const char* fic1,const char* fic2){
  std::cout<<"B : "<<pixB.valB<<std::endl;
  }
 */
+
+void test_kMean(int k,int niter,const char * fic1,const char* fic2){
+  imagePPM im1(fic1);
+  std::list<pixPPM>* groupes=im1.kMean(k,niter);
+  imagePPM sortie(k,groupes,im1.getHauteur(),im1.getLargeur());
+  sortie.EcrireImagePPM(fic2);
+}
+
+
 int main(int argc, char* argv[]){
   //testLecEcr(argv[1],argv[2]);
   //testPixelAddition();
   //testPixelDivision();
+
+  test_kMean(atoi(argv[1]),atoi(argv[2]),argv[3],argv[4]);
   return 0;
 }
