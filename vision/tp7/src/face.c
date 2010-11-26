fd65e9cef3d36ba09e72376ac290082758790402
nom <koraag@pcnt-217-12.(none)>
2010-11-26 18:27:48 +0100

    update

--- vision/tp7/src/face.c
+++ vision/tp7/src/face.c
@@ -17,50 +17,109 @@ float *histogram(int w, int h, int *image, char * filename)
 {
   int i, j, k,lum;
   float *hist;
+  float *imageFloat;
 
   printf("computation of histogram %s\n", filename);
 
-
+  imageFloat = (float *) malloc(w*h*3*sizeof(float));
   hist = (float *) malloc( 256 * 256 * sizeof(float));
   /* Initialisation de hist */
   for(i = 0;i<256;i++){
 	for(j=0;j<256;j++){
-	hist[i*256+j] = 0 ;
+	hist[i*256+j] = 0.0 ;
 	}	 
    }
 
   /* First normalize the image by the luminance */
 
-  for(i = 0; i < h ; i++)
+  for(i = 0; i < h ; i++){
     for(j = 0; j < w; j++)
       {
-
 	/* For each pixel, normalize its R, G, B values by its luminance */
 
 	/* TO BE COMPLETED */
-                          //  R(i,j)   //		//  G(i,j)  //			//  B(i,j)  //
-	lum = image[i * w * 3 + j * 3 + 0] + image[i * w * 3 + j * 3 + 1] + image[i * w * 3 + j * 3 + 2];
+  		  //  R(i,j)   //		//  G(i,j)  //			//  B(i,j)  //
+//	printf("R(i,j) : %i \n",image[i*w*3+j*3+0]);
+//	printf("G(i,j) : %i \n",image[i*w*3+j*3+1]);
+//	printf("B(i,j) : %i \n",image[i*w*3+j*3+2]);
+	lum =image[i * w * 3 + j * 3 + 0] + image[i * w * 3 + j * 3 + 1] + image[i * w * 3 + j * 3 + 2];
+//	printf("lum : %i \n",lum);
 
-	// On divise par R(i,j), G(i,j) et B(i,j) par la luminance lum
-	for(k=0;k<w;k++){
-	image[i * w * 3 +j * 3 +k] = image[i * w * 3 +j * 3 +k] / lum;	
-	printf("k %d\n",k);
-	printf("val %d\n", image[i*w*3+j*3+k]);
-	 printf("lum: %d\n", lum);
+	// On divise par R(i,j), G(i,j) et B(i,j) par la luminance lum	
+	for(k=0;k<3;k++){
+	  imageFloat[i*w*3+j*3+k] =  (float)image[i * w * 3 + j * 3 + k] / lum;
+//	  printf("val lumi normal : %f \n",imageFloat[i*w*3+j*3+k]);
+
 	}
+
 	/* then update the histogram in RG space */
 
 	/* TO BE COMPLETED */
 			//  R(i,j)   //			// G(i,j) 
-	hist[image[i * w * 3 + j * 3 + 0 ]*256 + image[i * w * 3 + j * 3 + 1 ] ] = hist[image[i * w * 3 + j * 3 + 0 ]*256 + image[i * w * 3 + j * 3 + 1 ]] + 1;
-	    
-       }
+	
+	
 
+	
+       }
+}
+	int a = 0;
+	int b = 255;
+	float min, max;
+
+	min=max=hist[0];
+	//printf("max : %f",max);
+	//printf("min : %f",min);
+	for(i=0;i<h;i++){
+		for(j=0;j<w;j++){
+		  for(k=0;k<3;k++){
+			//printf("val hist : %f \n",hist[i*256+j]);
+			if(min>imageFloat[i*w*3+j*3+k])min=imageFloat[i*w*3+j*3+k];
+			if(max<imageFloat[i*w*3+j*3+k])max=imageFloat[i*w*3+j*3+k];
+		  }
+		}
+	}
+	//printf("max : %f",max);
+	//printf("min : %f",min);
+	for(i=0;i<h;i++){
+		for(j=0;j<w;j++){
+		    for(k=0;k<3;k++){
+			 image[i*w*3+j*3+k]=(((double)imageFloat[i*w*3+j*3+k]-(double)min)/
+			              ((double)max-(double)min))*(b-a)+(double)a;
+			if(image[i*w*3+j*3+k]<256){
+			printf("val lumi normal [0,255] : %i \n",image[i*w*3+j*3+k]);
+			}
+			else{printf("PROBLEMEEEEEEEEEEEEEEEE \n");
+			      exit(-1);}
+		    }
+		}
+	}
+	printf("h : %i \n",h);
+	printf("w : %i \n",w);
+	int ind;
+	//printf("REGARDE ICI : %i \n",image[160*w*3+247*3+0]*256+image[160*w*3+247*3+1]);
+	for(i=0;i<h;i++){
+		for(j=0;j<w;j++){
+	      //  printf("ICI : %i \n",image[i*w*3+j*3+0]*256);
+	      //  printf("ICI 2 : %i \n",image[i*w*3+j*3+1]);
+		//printf("i : %i \n",i);
+		//printf("j : %i \n",j);
+		ind = image[i*w*3+j*3+0]*256 + image[i*w*3+j*3+1];
+		//printf("IND : %i \n",ind);
+		hist[ind]=hist[ind]+1.0;
+		//printf("val histo : %f\n",hist[i*256+j]);
+		}
+	}
   
+	for(i=0;i<256;i++){
+		for(j=0;j<256;j++){
+	//	printf("val histo : %f\n",hist[i*256+j]);
+		}
+	}
 	/* normalize the histogram	 */
-	  int sum = 0;
+/*	  int sum = 0;
 	  for(i = 0;i<256;i++){
 		for(j=0;j<256;j++){
+		//	printf("val hist : %f \n",hist[i*256+j]);
 			sum += hist[i*256+j];
 		}
 	  }
@@ -68,15 +127,39 @@ float *histogram(int w, int h, int *image, char * filename)
        	for(i = 0;i<256;i++){
 		for(j=0;j<256;j++){
 			hist[i*256+j]=hist[i*256+j]/sum;
-			 printf("sum: %d\n", sum);
 		}
-	}
+	}*/
  
   /* save the histrogram as an image .pgm */
-  
-  	 
- int x = writePixmap(hist, 256, 256, 5, filename);
-	   
+  /* Pour recadrer l'histogramme entre 0 et 255*/
+
+/*	int a = 0;
+	int b = 255;
+	float min, max;
+
+	min=max=hist[0];
+	//printf("max : %f",max);
+	//printf("min : %f",min);
+	for(i=0;i<256;i++){
+		for(j=0;j<256;j++){
+			//printf("val hist : %f \n",hist[i*256+j]);
+			if(min>hist[i*256+j])min=hist[i*256+j];
+			if(max<hist[i*256+j])max=hist[i*256+j];
+		}
+	}
+	//printf("max : %f",max);
+	//printf("min : %f",min);
+	for(i=0;i<256;i++){
+		for(j=0;j<256;j++){
+			hist[i*256+j]=(((double)hist[i*256+j]-(double)min)/
+			              ((double)max-(double)min))*(b-a)+(double)a;
+			//printf("val hist :%d \n",hist[i*256+j]);
+		}
+	}*/
+
+	 
+  writePixmap(hist, 256, 256, 5, filename);
+
   return hist;
 }
 
@@ -154,7 +237,7 @@ void main(int argc, char** argv)
   /* save the final image results : the image of probability 
      + the initial image with the barycenter */
 
-  writePixmap(result, cols, rows, 5, argv[3]);
+  writePixmap(result, cols, rows, 6, argv[3]);
 
 
   free(image);
